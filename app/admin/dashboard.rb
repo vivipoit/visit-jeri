f5c9a2577ffc20082c85c98f3509f8684ed01a7e
ViviPoit <vwpoit@gmail.com>
2019-08-16 15:53:08 -0400

    Build dashboard information

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,29 +4,80 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+        span 'Bem vindo ao Painel Administrativo.'
+        small 'No menu acima você faz a gestão dos seus conteúdos e traduções.'
+        small 'Nos paineis abaixo você verifica quais conteúdos já foram traduzidos.'
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      @content_locales = ContentLocale.all
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+      column do
+        panel 'Menus' do
+          Menu.all.map do |menu|
+            hr
+            strong menu.name
+            @content_locales.each do |cl|
+              div do
+                menu_locale = MenuLocale.where(menu: menu, content_locale: cl).first
+                if menu_locale.blank?
+                  status_tag false, label: 'Pendente'
+                else
+                  status_tag true, label: 'OK'
+                end
+                span cl.name
+              end
+            end
+            hr
+          end
+        end
+      end
+
+      column do
+        panel 'Páginas' do
+          Page.all.map do |page|
+            hr
+            strong page.name
+            small page.menu.name
+            @content_locales.each do |cl|
+              div do
+                page_locale = PageLocale.where(page: page, content_locale: cl).first
+                if page_locale.blank?
+                  status_tag false, label: 'Pendente'
+                else
+                  status_tag true, label: 'OK'
+                end
+                span cl.name
+              end
+            end
+            hr
+          end
+        end
+      end
+
+      column do
+        panel 'Itens' do
+          Item.all.map do |item|
+            hr
+            strong item.name
+            small item.page.name
+            @content_locales.each do |cl|
+              div do
+                item_locale = ItemLocale.where(item: item, content_locale: cl).first
+                if item_locale.blank?
+                  status_tag false, label: 'Pendente'
+                else
+                  status_tag true, label: 'OK'
+                end
+                span cl.name
+              end
+            end
+            hr
+          end
+        end
+      end
+
+    end
+  end
 end
