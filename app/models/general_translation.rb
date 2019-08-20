d9a1b6f8b517efab57e7e468dfcd97d7f30204e6
ViviPoit <vwpoit@gmail.com>
2019-08-20 10:45:05 -0400

    Implement I18n backend reload after saving new translations

--- app/models/general_translation.rb
+++ app/models/general_translation.rb
@@ -1,3 +1,10 @@
 class GeneralTranslation < ApplicationRecord
   self.table_name = 'translations'
+
+  after_save :reload_i18n_backend
+  after_destroy :reload_i18n_backend
+
+  def reload_i18n_backend
+    I18n.backend.reload!
+  end
 end
