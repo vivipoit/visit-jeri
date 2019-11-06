244a00a53a8750b344e6561dc62f2e2720af6422
Vivi Poit <vwpoit@gmail.com>
2019-11-06 10:39:56 +0630

    Update Ruby

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-
+Gem::Specification.find_by_name('bundler').activate
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
