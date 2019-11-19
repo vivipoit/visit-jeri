b68ffb291a44cd2a98d90a69482ae3a1fe96b5ae
Vivi Poit <vwpoit@gmail.com>
2019-11-19 16:33:48 +0630

    Apply rubocop suggestions to item_locale

--- app/models/item_locale.rb
+++ app/models/item_locale.rb
@@ -6,17 +6,18 @@ class ItemLocale < ApplicationRecord
 
   before_save :parameterize_link
 
-  scope :show_info, -> (content_locale_id, link) {
-    where(content_locale_id: content_locale_id, link: link).first
-  }
+  scope :show_info, ->(content_locale_id, link) { find_by(content_locale_id: content_locale_id, link: link) }
 
-  scope :home_info, -> (content_locale_id) {
-    includes(item: [page: [:page_locales]]).where(content_locale_id: content_locale_id, item: {page: {page_locales: {content_locale_id: content_locale_id}}}).order(updated_at: :desc).limit(3)
-  }
+  def self.home_info(content_locale_id)
+    includes(item: [page: [:page_locales]])
+      .where(content_locale_id: content_locale_id, item: { page: { page_locales: { content_locale_id: content_locale_id } } })
+      .order(updated_at: :desc)
+      .limit(3)
+  end
 
   private
 
   def parameterize_link
-    self.link = self.link.parameterize
+    self.link = link.parameterize
   end
 end
