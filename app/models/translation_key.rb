d6f945794bcbf8d093bb4619280726e8b5cdedaf
Vivi Poit <vwpoit@gmail.com>
2019-11-22 11:47:21 +0630

    Apply rubocop suggestions to translation_key model

--- app/models/translation_key.rb
+++ app/models/translation_key.rb
@@ -2,17 +2,26 @@
 
 class TranslationKey
   def self.list
-    [
-      'home_small_print',
-      'home_main_title',
-      'more_info',
-      'from_the_people_who_have_come',
-      'contact_us',
-      'eager_to_help',
-      'thanks',
-      'send_form_info',
-      'all_rights_reserved',
-      'jeri_ce_brasil'
+    home + footer
+  end
+
+  def self.home
+    %w[
+      home_small_print
+      home_main_title
+      more_info
+      from_the_people_who_have_come
+    ]
+  end
+
+  def self.footer
+    %w[
+      contact_us
+      eager_to_help
+      thanks
+      send_form_info
+      all_rights_reserved
+      jeri_ce_brasi
     ]
   end
 end
