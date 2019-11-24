5089e647901c3c322b3fe62d6613096bd0714ade
Vivi Poit <vwpoit@gmail.com>
2019-11-24 13:20:06 +0630

    Apply rubocop suggestions to page_locale.rb

--- app/models/page_locale.rb
+++ app/models/page_locale.rb
@@ -1,25 +1,20 @@
 # frozen_string_literal: true
 
 class PageLocale < ApplicationRecord
-  belongs_to :page
-  belongs_to :content_locale
-
   extend FriendlyId
   friendly_id :link
 
-  scope :show_info, ->(content_locale_id, link) {
-    includes(:page).where(content_locale_id: content_locale_id, link: link).first
-  }
-
-  scope :home_info, ->(content_locale_id) {
-    includes(:page).where(content_locale_id: content_locale_id).order(updated_at: :desc).limit(3)
-  }
+  belongs_to :page
+  belongs_to :content_locale
 
   before_save :parameterize_link
+
+  scope :show_info, ->(content_locale_id, link) { includes(:page).find_by(content_locale_id: content_locale_id, link: link) }
+  scope :home_info, ->(content_locale_id) { includes(:page).where(content_locale_id: content_locale_id).order(updated_at: :desc).limit(3) }
 
   private
 
   def parameterize_link
-    self.link = self.link.parameterize
+    self.link = link.parameterize
   end
 end
