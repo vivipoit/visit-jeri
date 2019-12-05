0c814625f0ab4c8eaeb0048e2c8217c5185d1ee9
Vivi Poit <vwpoit@gmail.com>
2019-12-05 09:38:07 +0630

    Apply rubocop suggestions to menu and item models

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,11 @@
 
 class Item < ApplicationRecord
   belongs_to :page
-  has_many :item_locales
+  has_many :item_locales, dependent: :destroy
 
-  scope :for_this_locale, ->(content_locale_id) {
-    includes(:item_locales).where(item_locales: { content_locale_id: content_locale_id }).references(:item_locales)
+  scope :for_this_locale, lambda { |content_locale_id|
+    includes(:item_locales)
+      .where(item_locales: { content_locale_id: content_locale_id })
+      .references(:item_locales)
   }
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class Menu < ApplicationRecord
-  has_many :menu_locales
-  has_many :pages
+  has_many :menu_locales, dependent: :destroy
+  has_many :pages, dependent: :destroy
 
-  scope :build_navbar, ->(content_locale_id) {
-    includes(:menu_locales, pages: [:page_locales]).where(menu_locales: { content_locale_id: content_locale_id }, pages: { page_locales: { content_locale_id: content_locale_id } }).references(:menu_locales)
+  scope :build_navbar, lambda { |content_locale_id|
+    includes(:menu_locales, pages: [:page_locales])
+      .where(
+        menu_locales: { content_locale_id: content_locale_id },
+        pages: { page_locales: { content_locale_id: content_locale_id } }
+      )
+      .references(:menu_locales)
   }
 end
